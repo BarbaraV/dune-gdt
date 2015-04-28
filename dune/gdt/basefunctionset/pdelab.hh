49eb1d5c81672c318962646ba3cd73dfcfc8f55b
b_verf01 <b_verf01@uni-muenster.de>
2015-04-28 09:42:15 +0200

    added an Hcurl-conforming transformation

--- dune/gdt/basefunctionset/pdelab.hh
+++ dune/gdt/basefunctionset/pdelab.hh
@@ -42,6 +42,14 @@ class PiolaTransformedPdelabWrapper
   static_assert(Dune::AlwaysFalse< PdelabSpaceImp >::value, "Untested for these dimensions!");
 };
 
+//new
+template< class PdelabSpaceImp, class EntityImp,
+          class DomainFieldImp, size_t domainDim,
+          class RangeFieldImp, size_t rangeDim, size_t rangeDimCols = 1 >
+class HcurlTransformedPdelabWrapper
+{
+  static_assert(Dune::AlwaysFalse< PdelabSpaceImp >::value, "Untested for these dimensions!");
+};
 
 namespace internal {
 
@@ -89,6 +97,31 @@ public:
   typedef EntityImp EntityType;
 private:
   friend class PiolaTransformedPdelabWrapper < PdelabSpaceImp, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1 >;
+};
+
+//new
+/** \brief Traits for the class HcurlTransformedPdelabWrapper
+ *
+ * \sa HcurlTransformedPdelabWrapper
+*/
+template< class PdelabSpaceImp, class EntityImp,
+          class DomainFieldImp, size_t domainDim,
+          class RangeFieldImp, size_t rangeDim >
+class HcurlTransformedPdelabWrapperTraits
+{
+  static_assert(domainDim == rangeDim, "Untested!");
+  static_assert(domainDim == 3, "Untested!");
+public:
+  typedef HcurlTransformedPdelabWrapper < PdelabSpaceImp, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim >
+      derived_type;
+private:
+  typedef PDELab::LocalFunctionSpace< PdelabSpaceImp, PDELab::TrialSpaceTag > PdelabLFSType;
+  typedef FiniteElementInterfaceSwitch< typename PdelabSpaceImp::Traits::FiniteElementType > FESwitchType;
+public:
+  typedef typename FESwitchType::Basis BackendType;
+  typedef EntityImp EntityType;
+private:
+  friend class HcurlTransformedPdelabWrapper < PdelabSpaceImp, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1 >;
 };
 
 
@@ -299,6 +332,131 @@ private:
   mutable std::vector< JacobianRangeType > tmp_jacobian_ranges_;
 }; // class PiolaTransformedPdelabWrapper
 
+//new
+/** \brief Wrapper-class for the Hcurl-confroming transformation, e.g. for member functions of the Nedelec spaces
+ *
+ * \note As the curl is only defined in dimension 3, the domainDim and the rangeDim have to equal 3
+ *
+ * \tparam PdelabSpaceType Type of function space, has to be implemented in dune-pdelab/finiteelementmap
+ * \tparam EntityImp Type of Entity the transformation maps from
+ * \tparam DomainFieldImp Type of the domain field
+ * \tparam domainDim Dimension of the domain, has to be 3
+ * \tparam RangeFieldImp Type of the range field
+ * \tparam rangeDim Dimension of the range, has to be 3
+ */
+template< class PdelabSpaceType, class EntityImp,
+          class DomainFieldImp, size_t domainDim,
+          class RangeFieldImp, size_t rangeDim >
+class HcurlTransformedPdelabWrapper< PdelabSpaceType, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1 >
+  : public BaseFunctionSetInterface< internal::HcurlTransformedPdelabWrapperTraits< PdelabSpaceType, EntityImp,
+                                                                       DomainFieldImp, domainDim,
+                                                                       RangeFieldImp, rangeDim >,
+                                     DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1 >
+{
+  typedef HcurlTransformedPdelabWrapper
+    < PdelabSpaceType, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1 > ThisType;
+  typedef BaseFunctionSetInterface< internal::PiolaTransformedPdelabWrapperTraits< PdelabSpaceType, EntityImp,
+                                                                      DomainFieldImp, domainDim,
+                                                                      RangeFieldImp, rangeDim >,
+                                    DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1 >
+      BaseType;
+public:
+  typedef internal::HcurlTransformedPdelabWrapperTraits< PdelabSpaceType, EntityImp,
+                                            DomainFieldImp, domainDim,
+                                            RangeFieldImp, rangeDim >
+                                         Traits;
+  typedef typename Traits::BackendType   BackendType;
+  typedef typename Traits::EntityType    EntityType;
+private:
+  typedef typename Traits::PdelabLFSType PdelabLFSType;
+  typedef typename Traits::FESwitchType  FESwitchType;
+
+public:
+  using typename BaseType::DomainFieldType;
+  using BaseType::dimDomain;
+  using typename BaseType::DomainType;
+  using typename BaseType::RangeType;
+  using typename BaseType::JacobianRangeType;
+
+  HcurlTransformedPdelabWrapper(const PdelabSpaceType& space, const EntityType& ent)
+    : BaseType(ent)
+    , tmp_domain_(DomainFieldType(0))
+    , tmp_jacobian_transposed_(DomainFieldType(0))
+    , tmp_jacobian_inverse_transposed_(DomainFieldType(0))
+  {
+    PdelabLFSType* lfs_ptr = new PdelabLFSType(space);
+    lfs_ptr->bind(this->entity());
+    lfs_ = std::unique_ptr< PdelabLFSType >(lfs_ptr);
+    backend_ = std::unique_ptr< BackendType >(new BackendType(FESwitchType::basis(lfs_->finiteElement())));
+    tmp_ranges_ = std::vector< RangeType >(backend_->size(), RangeType(0));
+    tmp_jacobian_ranges_ = std::vector< JacobianRangeType >(backend_->size(), JacobianRangeType(0));
+  } // PdelabWrapper(...)
+
+  HcurlTransformedPdelabWrapper(ThisType&& source) = default;
+
+  HcurlTransformedPdelabWrapper(const ThisType& /*other*/) = delete;
+
+  ThisType& operator=(const ThisType& /*other*/) = delete;
+
+  const BackendType& backend() const
+  {
+    return *backend_;
+  }
+
+  virtual size_t size() const override final
+  {
+    return backend_->size();
+  }
+
+  virtual size_t order() const override final
+  {
+    return backend_->order();
+  }
+
+  virtual void evaluate(const DomainType& xx, std::vector< RangeType >& ret) const override final
+  {
+    assert(lfs_);
+    assert(backend_);
+    assert(tmp_ranges_.size() >= backend_->size());
+    assert(ret.size() >= backend_->size());
+    backend_->evaluateFunction(xx, tmp_ranges_);
+    const auto geometry = this->entity().geometry();
+    tmp_jacobian_inverse_transposed_ = geometry.jacobianInverseTransposed(xx);
+    for (size_t ii = 0; ii < backend_->size(); ++ii) {
+      tmp_jacobian_inverse_transposed_.mv(tmp_ranges_[ii], ret[ii]);  //stimmt das?
+    }
+  } // ... evaluate(...)
+
+  using BaseType::evaluate;
+
+  virtual void jacobian(const DomainType& xx, std::vector< JacobianRangeType >& ret) const override final
+  {
+    assert(lfs_);
+    assert(backend_);
+    assert(ret.size() >= backend_->size());
+    backend_->evaluateJacobian(xx, tmp_jacobian_ranges_);
+    const auto geometry = this->entity().geometry();
+    tmp_jacobian_transposed_ = geometry.jacobianTransposed(xx);
+    const DomainFieldType integration_element = geometry.integrationElement(xx);
+    for (size_t ii = 0; ii < backend_->size(); ++ii) {
+      for (size_t jj = 0; jj < dimDomain; ++jj) {
+        tmp_jacobian_transposed_.mtv(tmp_jacobian_ranges_[ii][jj], ret[ii][jj]);
+        ret[ii][jj] /= integration_element;    //stimmt das?
+      }
+    }
+  } // ... jacobian(...)
+
+  using BaseType::jacobian;
+
+private:
+  mutable DomainType tmp_domain_;
+  mutable typename EntityType::Geometry::JacobianTransposed tmp_jacobian_transposed_;
+  mutable typename EntityType::Geometry::JacobianInverseTransposed tmp_jacobian_inverse_transposed_;
+  std::unique_ptr< const PdelabLFSType > lfs_;
+  std::unique_ptr< const BackendType > backend_;
+  mutable std::vector< RangeType > tmp_ranges_;
+  mutable std::vector< JacobianRangeType > tmp_jacobian_ranges_;
+}; // class HcurlTransformedPdelabWrapper
 
 #else // HAVE_DUNE_PDELAB
 
@@ -320,6 +478,14 @@ class PiolaTransformedPdelabWrapper
   static_assert(AlwaysFalse< PdelabSpaceImp >::value, "You are missing dune-pdelab!");
 };
 
+//new
+template< class PdelabSpaceImp, class EntityImp,
+          class DomainFieldImp, size_t domainDim,
+          class RangeFieldImp, size_t rangeDim, size_t rangeDimCols = 1 >
+class HcurlTransformedPdelabWrapper
+{
+  static_assert(AlwaysFalse< PdelabSpaceImp >::value, "You are missing dune-pdelab!");
+};
 
 #endif // HAVE_DUNE_PDELAB
 
