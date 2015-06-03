b18bd93ca48e972043265ac7708047741ac4014a
b_verf01 <b_verf01@uni-muenster.de>
2015-06-03 11:12:39 +0200

    [test.curlcurl-discretization.cc] simple tests for the discretization with visualization and error computation

--- test/curlcurl-discretization.cc
+++ test/curlcurl-discretization.cc
@@ -9,65 +9,71 @@
 #include <string>
 
 
-#include <dune/grid/yaspgrid.hh>
+#include <dune/grid/alugrid.hh>
 
-#include <dune/stuff/functions/combined.hh>
+
+#include <dune/stuff/functions/global.hh>
+#include <dune/stuff/functions/constant.hh>
 #include <dune/stuff/functions/expression.hh>
+#include <dune/stuff/grid/provider/cube.hh>
 
-#include <dune/gdt/localevaluation/curlcurl.hh>
-#include <dune/gdt/localevaluation/elliptic.hh>
+#include <dune/gdt/products/l2.hh>
+#include <dune/gdt/products/hcurl.hh>
 
-#include <dune/gdt/spaces/nedelec/pdelab.hh>
+#include "curlcurldiscretization.hh"
+
+using namespace Dune;
 
 
 int main() {
- //   std::cout << "Hallo" <<std::endl;
-    //dune::Yaspgrid erzeugen
-    Dune::array< int, 3> n;
-    std::fill(n.begin(), n.end(), 5);
-    Dune::FieldVector< double, 3> upper(1.0);
-    typedef Dune::YaspGrid< 3 > GridType;
-    GridType grid(upper, n);
-    typedef GridType::LeafGridView LeafGridView;
-    LeafGridView leafView = grid.leafGridView();
-    typedef LeafGridView::template Codim< 0 >::Iterator ElementLeafIterator;
-    ElementLeafIterator it = leafView.template begin< 0 >();
-    typedef GridType::Codim< 0 >::Entity EntityType;
-
-    //Fkt addieren und multiplizieren
-    typedef Dune::Stuff::Functions::Expression< EntityType, double, 3, double, 1 > ExpressionFct;
-    typedef Dune::Stuff::Functions::Constant< EntityType, double, 3, double , 1 > ConstFct;
-    ExpressionFct myfunction("x", "x[0]+x[1]", 1);
-   // ExpressionFctType a("x", "3.0", 0);
-    ConstFct a(1.0);
-    auto newfct = myfunction+a;
-    Dune::Stuff::Functions::Product< ConstFct, ExpressionFct > newfct2(a, myfunction);
-//    typedef Dune::Stuff::Functions::Expression< EntityType, double, 3, std::complex< double >, 1> ComplexExpressionFct;
-//    ComplexExpressionFct mycfct("x", "x[0]+i*x[1]", 1);
-    //Fazit: geht genau so, wie es hier steht. RangeFiled muss bei beiden Fkt natuerlich gleich sein. Geht aber nicht fuer komplexe Fkt, da Fktsauswertungen
-    // bzw. Ueberpruefungen gegen unedlich etc. (ohne Betrag) ausgefuehrt werden
-    // es sollten die Parameterfkt fertig an curlcurldiskretisierung ubergeben werden!
-    // Problem: kann noch nicht einmal komplex-wertige Expressionfkt anlegen, da deren evaluate-Methode std::isnan und std::isinf benutzt!!!
+  // instantiate alugrid
+  typedef Dune::ALUGrid< 3, 3, simplex, conforming > GridType;
+  Stuff::Grid::Providers::Cube< GridType > grid_provider(0.0, 1.0, 4);
+  auto& grid = grid_provider.grid();
+  typedef GridType::LeafGridView LeafGridView;
+  auto leafView = grid.leafGridView();
+  typedef LeafGridView::Codim< 0 >::Entity EntityType;
 
 
-    //local evaluation curl curl testen
-    const ConstFct permeab(1.0);\
-    typedef Dune::Stuff::Functions::Expression< EntityType, double, 3, double, 3 > ExpressionFctVector;
-    const std::vector< std::string > expressions{"0", "0", "x[0]"};
-    const std::vector< std::vector< std::string > > gradients{{"0", "0", "0"}, {"0", "0", "0"}, {"1", "0", "0"}};
-    const ExpressionFctVector testfct("x", expressions, 1, "stuff.globalfunction.expression", gradients);
-    const std::vector< std::string > expressions1{"0", "0", "-x[0]"};
-    const std::vector< std::vector< std::string > > gradients1{{"0", "0", "0"}, {"0", "0", "0"}, {"-1", "0", "0"}};
-    const ExpressionFctVector testfct1("x", expressions1, 1, "stuff.globalfunction.expression", gradients1);
-    const Dune::FieldVector< double, 3 > localpoint(0.5);
-    Dune::DynamicMatrix< double > ret(1, 1, 0.0);
 
-    Dune::GDT::LocalEvaluation::CurlCurl< ConstFct > localeval(permeab);
-    auto permeabtuple = localeval.localFunctions(*it);
-    localeval.evaluate(permeabtuple, *(testfct.local_function(*it)), *(testfct1.local_function(*it)), localpoint, ret);
-    std::cout << ret <<std::endl;
-    //funktioniert!!! :-)
+  //test real discretization
+  typedef Stuff::Functions::Expression< EntityType, double, 3, double, 3 > ExpressionFct;
+  typedef Stuff::Functions::Constant< EntityType, double, 3, double, 1 >  ConstantFct;
+  typedef Stuff::Functions::Constant< EntityType, double, 3, double, 3> VectorFct;
 
+  const ConstantFct scalar1(1.0);
+  const ConstantFct scalar2(-1.0);
+  const ExpressionFct expsol("x", {"sin(pi*x[1])*sin(pi*x[2])", "sin(pi*x[0])*sin(pi*x[2])", "sin(pi*x[0])*sin(pi*x[1])"}, 2, "expectedsolution",
+                             {{"0", "pi*cos(pi*x[1])*sin(pi*x[2])", "pi*sin(pi*x[1])*cos(pi*x[2])"},
+                              {"pi*cos(pi*x[0])*sin(pi*x[2])", "0", "pi*sin(pi*x[0])*cos(pi*x[2])"},
+                              {"pi*cos(pi*x[0])*sin(pi*x[1])", "pi*sin(pi*x[0])*cos(pi*x[1])", "0"}});
+  const ConstantFct realweight(2*pi*pi-1.0);   //2*pi^2+scalar2
+  const ConstantFct imagweight(1.0);           //scalar1
+
+  const Stuff::Functions::Product< ConstantFct, ExpressionFct > freal(realweight, expsol);
+  const Stuff::Functions::Product< ConstantFct, ExpressionFct > fimag(imagweight, expsol);
+
+
+  DSG::BoundaryInfos::AllDirichlet< LeafGridView::Intersection > bdry_info;
+  Discretization< LeafGridView, 1> realdiscr(leafView, bdry_info, scalar1, scalar2, scalar1, freal, fimag);
+  typedef std::complex< double > complextype;
+  Dune::Stuff::LA::Container< complextype >::VectorType sol;
+  realdiscr.solve(sol);
+  //realdiscr.visualize(sol, "discrete_solution", "discrete_solution");
+
+  typedef Stuff::LA::Container< double, Stuff::LA::ChooseBackend::eigen_sparse>::VectorType VectorType;
+  typedef GDT::Spaces::Nedelec::PdelabBased< LeafGridView, 1, double, 3> SpaceType;
+  typedef GDT::ConstDiscreteFunction< SpaceType, VectorType > ConstDiscreteFct;
+  Stuff::LA::Container< double >::VectorType solreal(sol.size());
+  solreal.backend() = sol.backend().real();
+  ConstDiscreteFct solasfunc(realdiscr.space(), solreal, "solution_as_discrete_function");
+
+  //error computation
+  typedef Stuff::Functions::Difference< ExpressionFct, ConstDiscreteFct > DifferenceFct;
+  DifferenceFct myerror(expsol, solasfunc);
+  GDT::Products::L2< LeafGridView> l2_product_operator(leafView);
+  GDT::Products::HcurlSemi< LeafGridView > hcurl_product_operator(leafView);
+  //  std::cout<< std::sqrt(l2_product_operator.apply2(myerror, myerror) + hcurl_product_operator.apply2(myerror, myerror))<< std::endl;
 
     return 0;
 }
