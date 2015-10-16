42c50116f47ba29499ba463faabde64fab2058bb
b_verf01 <b_verf01@uni-muenster.de>
2015-10-16 15:03:23 +0200

    implement a Hemlholtz decomposition (gradient part) as solution of the corresponding elliptic dirichlet problem

--- test/hmm-discretization.hh
+++ test/hmm-discretization.hh
@@ -37,6 +37,10 @@
 #include <dune/gdt/functionals/l2.hh>
 #include <dune/gdt/spaces/constraints.hh>
 
+//for HelmholtzDecomp
+#include <dune/gdt/spaces/cg/pdelab.hh>
+#include <dune/gdt/operators/elliptic-cg.hh>
+
 template< class MacroGridViewType, class CellGridPartType, int polynomialOrder, class MacroParamImp = void >
 class HMMDiscretization
 {
@@ -251,6 +255,154 @@ private:
   mutable VectorType rhs_vector_real_;
   mutable VectorType rhs_vector_imag_;
   mutable ComplexVectorType rhs_vector_total_;
+};
+
+
+template< class GridViewImp, int polynomialOrder >
+class HelmholtzDecomp {
+public:
+  typedef typename GridViewImp::ctype DomainFieldType;
+  static const size_t dimDomain = GridViewImp::dimension;
+  static const unsigned int polOrder = polynomialOrder;
+
+  typedef Dune::Stuff::Grid::BoundaryInfoInterface< typename GridViewImp::Intersection > BdryInfoType;
+  typedef Dune::Stuff::Functions::Constant< typename GridViewImp::template Codim<0>::Entity, DomainFieldType, dimDomain, double, 1 > ConstantFct;
+  typedef Dune::Stuff::Functions::Constant< typename GridViewImp::template Codim<0>::Entity, DomainFieldType, dimDomain, double, dimDomain > ConstantVectorFct;
+  typedef Dune::Stuff::LocalizableFunctionInterface< typename GridViewImp::template Codim< 0 >::Entity, DomainFieldType, dimDomain, double, dimDomain > VectorFct;
+  typedef Dune::Stuff::LocalizableFunctionInterface< typename GridViewImp::template Codim< 0 >::Entity, DomainFieldType, dimDomain, double, 1 >        ScalarFct;
+
+
+  typedef Dune::Stuff::LA::Container< double, Dune::Stuff::LA::ChooseBackend::eigen_sparse>::MatrixType                 MatrixType;
+  typedef Dune::Stuff::LA::Container< double, Dune::Stuff::LA::ChooseBackend::eigen_sparse>::VectorType                 VectorType;
+  typedef Dune::Stuff::LA::Container< std::complex< double >, Dune::Stuff::LA::ChooseBackend::eigen_sparse>::MatrixType MatrixTypeComplex;
+  typedef Dune::Stuff::LA::Container< std::complex< double >, Dune::Stuff::LA::ChooseBackend::eigen_sparse>::VectorType VectorTypeComplex;
+
+  typedef Dune::GDT::Spaces::CG::PdelabBased< GridViewImp, polOrder, double, 1 > SpaceType;
+  typedef Dune::GDT::ConstDiscreteFunction< SpaceType, VectorType > DiscreteFctType;
+
+
+  HelmholtzDecomp(const GridViewImp& gp,
+                  const BdryInfoType& info,
+                  const VectorFct& sourcereal,
+                  const VectorFct& sourceimag = ConstantVectorFct(0.0),
+                  const ScalarFct& param = ConstantFct(1.0))
+    : space_(gp)
+    , boundary_info_(info)
+    , source_real_(sourcereal)
+    , source_imag_(sourceimag)
+    , param_(param)
+    , is_assembled_(false)
+    , system_matrix_(0,0)
+    , system_matrix_complex_(0,0)
+    , rhs_vector_real_(0)
+    , rhs_vector_imag_(0)
+    , rhs_vector_total_(0)
+  {}
+
+  const SpaceType& space() const
+  {
+    return space_;
+  }
+
+  VectorType create_vector() const
+  {
+    return VectorType(space_.mapper().size());
+  }
+
+  VectorTypeComplex create_complex_vector() const
+  {
+    return VectorTypeComplex(space_.mapper().size());
+  }
+
+  void assemble() const
+  {
+    using namespace Dune;
+    using namespace Dune::GDT;
+    if (!is_assembled_) {
+      //prepare
+      Stuff::LA::SparsityPatternDefault sparsity_pattern = space_.compute_volume_pattern();
+      system_matrix_ = MatrixType(space_.mapper().size(), space_.mapper().size(), sparsity_pattern);
+      system_matrix_complex_ = MatrixTypeComplex(space_.mapper().size(), space_.mapper().size());
+      rhs_vector_real_ = VectorType(space_.mapper().size());
+      rhs_vector_imag_ = VectorType(space_.mapper().size());
+      rhs_vector_total_ = VectorTypeComplex(space_.mapper().size());
+      SystemAssembler< SpaceType > grid_walker(space_);
+
+      //lhs
+      typedef Operators::EllipticCG< ScalarFct, MatrixType, SpaceType > EllipticOpType;
+      EllipticOpType elliptic_operator(param_, system_matrix_, space_);
+      grid_walker.add(elliptic_operator);
+
+      //rhs
+      typedef LocalFunctional::Codim0Integral< LocalEvaluation::L2grad< VectorFct > > L2gradOp;
+      const L2gradOp l2gradreal(source_real_);
+      const L2gradOp l2gradimag(source_imag_);
+      typedef LocalAssembler::Codim0Vector< L2gradOp > VectorAssType;
+      VectorAssType vectorassembler1(l2gradreal);
+      VectorAssType vectorassembler2(l2gradimag);
+      grid_walker.add(vectorassembler1, rhs_vector_real_);
+      grid_walker.add(vectorassembler2, rhs_vector_imag_);
+
+      //dirichlet constraints
+      Spaces::DirichletConstraints< typename GridViewImp::Intersection >
+              dirichlet_constraints(boundary_info_, space_.mapper().size());
+      grid_walker.add(dirichlet_constraints);
+      grid_walker.assemble();
+
+      //build complex matrix and rhs
+      std::complex< double > im(0.0, 1.0);
+      system_matrix_complex_.backend() = system_matrix_.backend().template cast<std::complex< double > >();
+      rhs_vector_total_.backend() = rhs_vector_imag_.backend().template cast<std::complex< double > >();
+      rhs_vector_total_.scal(im);
+      rhs_vector_total_.backend() += rhs_vector_real_.backend().template cast< std::complex< double > >();
+
+      dirichlet_constraints.apply(system_matrix_complex_, rhs_vector_total_);
+      is_assembled_ = true;
+    }
+  } //assemble()
+
+  bool assembled() const
+  {
+    return is_assembled_;
+  }
+
+
+  const MatrixTypeComplex& system_matrix() const
+  {
+    return system_matrix_complex_;
+  }
+
+
+  const VectorTypeComplex& rhs_vector() const
+  {
+    return rhs_vector_total_;
+  }
+
+  void solve(VectorTypeComplex& solution) const
+  {
+    if(!is_assembled_)
+      assemble();
+
+    // instantiate solver and options
+    typedef Dune::Stuff::LA::Solver< MatrixTypeComplex > SolverType;
+    SolverType solver(system_matrix_complex_);
+    //solve
+    solver.apply(rhs_vector_total_, solution, "bicgstab.diagonal");
+  } //solve()
+
+
+private:
+  const SpaceType           space_;
+  const BdryInfoType&       boundary_info_;
+  const VectorFct&          source_real_;
+  const VectorFct&          source_imag_;
+  const ScalarFct&          param_;
+  mutable bool              is_assembled_;
+  mutable MatrixType        system_matrix_;
+  mutable MatrixTypeComplex system_matrix_complex_;
+  mutable VectorType        rhs_vector_real_;
+  mutable VectorType        rhs_vector_imag_;
+  mutable VectorTypeComplex rhs_vector_total_;
 };
 
 #endif // DUNE_GDT_TEST_HMM_DISCRETIZATION_HH
