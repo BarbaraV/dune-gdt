cf458d55d82bd03969ae73c51e86bdbf10e583b6
b_verf01 <b_verf01@uni-muenster.de>
2015-11-05 16:07:52 +0100

    add a codim0 integral for hmm evaluations

---
+++ dune/gdt/localoperator/hmmcodim0.hh
@@ -0,0 +1,136 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_GDT_LOCALOPERATOR_HMMCODIM0_HH
+#define DUNE_GDT_LOCALOPERATOR_HMMCODIM0_HH
+
+#include <vector>
+#include <utility>
+#include <type_traits>
+#include <limits>
+
+#include <boost/numeric/conversion/cast.hpp>
+
+#include <dune/common/densematrix.hh>
+
+#include <dune/geometry/quadraturerules.hh>
+
+#include <dune/stuff/functions/interfaces.hh>
+
+#include "interface.hh"
+
+namespace Dune {
+namespace GDT {
+namespace LocalOperator {
+
+
+//forward to be used in the traits
+template< class HMMEvaluation >
+class HMMCodim0Integral;
+
+
+namespace internal {
+
+
+template< class HMMEvaluation >
+class HMMCodim0IntegralTraits
+{
+public:
+  typedef HMMCodim0Integral< HMMEvaluation > derived_type;
+};
+
+
+} //namespace internal
+
+
+template< class HMMEvaluation >
+class HMMCodim0Integral
+//  : public LocalOperator::Codim0Interface< internal::HMMCodim0IntegralTraits< HMMEvaluation > >  //would like to derive like this, but method apply does not fit.
+{
+public:
+  typedef internal::HMMCodim0IntegralTraits< HMMEvaluation > Traits;
+  typedef HMMEvaluation                                      HMMEvaluationType;
+
+private:
+  static const size_t numTmpObjectsRequired_ = 1;
+
+public:
+  template< class... Args >
+  explicit HMMCodim0Integral(Args&& ...args)
+    : evaluation_(std::forward< Args >(args)...)
+    , over_integrate_(0)
+  {}
+
+  template< class... Args >
+  explicit HMMCodim0Integral(const int over_integrate, Args&& ...args)     // to do: add member grid_view
+    : evaluation_(std::forward< Args >(args)...)
+    , over_integrate_(boost::numeric_cast< size_t >(over_integrate))
+  {}
+
+  template< class... Args >
+  explicit HMMCodim0Integral(const size_t over_integrate, Args&& ...args)
+    : evaluation_(std::forward< Args >(args)...)
+    , over_integrate_(over_integrate)
+  {}
+
+  size_t numTmpObjectsRequired() const
+  {
+    return numTmpObjectsRequired_;
+  }
+
+  template< class E, class D, size_t d, class R, size_t rT, size_t rCT, size_t rA, size_t rCA >
+  void apply(const Stuff::LocalfunctionSetInterface< E, D, d, R, rT, rCT >& testBase,
+             const Stuff::LocalfunctionSetInterface< E, D, d, R, rA, rCA >& ansatzBase,
+             const size_t& entity_index,
+             Dune::DynamicMatrix< R >& ret,
+             std::vector< Dune::DynamicMatrix< R > >& tmpLocalMatrices) const
+  {
+    const auto& entity = ansatzBase.entity();                            //to do: get index of the entity from the member grid_view
+    const auto localFunctions = evaluation_.localFunctions(entity);
+    // quadrature
+    typedef Dune::QuadratureRules< D, d > VolumeQuadratureRules;
+    typedef Dune::QuadratureRule< D, d > VolumeQuadratureType;
+    const size_t integrand_order = evaluation_.order(localFunctions, ansatzBase, testBase) + over_integrate_;
+    const VolumeQuadratureType& volumeQuadrature = VolumeQuadratureRules::rule(entity.type(),
+                                                                               boost::numeric_cast< int >(integrand_order));
+    // check matrix and tmp storage
+    const size_t rows = testBase.size();
+    const size_t cols = ansatzBase.size();
+    ret *= 0.0;
+    assert(ret.rows() >= rows);
+    assert(ret.cols() >= cols);
+    assert(tmpLocalMatrices.size() >= numTmpObjectsRequired_);
+    auto& evaluationResult = tmpLocalMatrices[0];
+    // loop over all quadrature points
+    const auto quadPointEndIt = volumeQuadrature.end();
+    size_t kk = 0;
+    for (auto quadPointIt = volumeQuadrature.begin(); quadPointIt != quadPointEndIt; ++quadPointIt, ++kk) {
+      const Dune::FieldVector< D, d > x = quadPointIt->position();
+      // integration factors
+      const double integrationFactor = entity.geometry().integrationElement(x);
+      const double quadratureWeight = quadPointIt->weight();
+      // evaluate the local operation
+      auto key = std::make_pair(entity_index, kk);
+      evaluation_.evaluate(localFunctions, ansatzBase, testBase, x, key, evaluationResult);
+      // compute integral
+      for (size_t ii = 0; ii < rows; ++ii) {
+        auto& retRow = ret[ii];
+        const auto& evaluationResultRow = evaluationResult[ii];
+        for (size_t jj = 0; jj < cols; ++jj)
+          retRow[jj] += evaluationResultRow[jj] * integrationFactor * quadratureWeight;
+      } // compute integral
+    } // loop over all quadrature points
+  } // ... apply(...)
+
+private:
+  const HMMEvaluation evaluation_;
+  const size_t over_integrate_;
+}; //class HMMCodim0Integral
+
+} //namespace LocalOperator
+} //namespace GDT
+} //namespace Dune
+
+#endif // DUNE_GDT_LOCALOPERATOR_HMMCODIM0_HH
