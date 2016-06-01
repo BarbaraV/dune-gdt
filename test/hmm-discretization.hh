c9cb2e7f4a9b8c8dd9e97a9d5e7d2f27d6f7ab2c
b_verf01 <b_verf01@uni-muenster.de>
2016-06-01 15:02:47 +0200

    added first draft of DeltaCorrectorCurl

--- test/hmm-discretization.hh
+++ test/hmm-discretization.hh
@@ -41,6 +41,9 @@
 #include <dune/gdt/functionals/l2.hh>
 #include <dune/gdt/spaces/constraints.hh>
 
+//for DeltaCorrector
+#include <dune/stuff/grid/search.hh>
+
 //for HelmholtzDecomp
 #include <dune/gdt/spaces/cg/pdelab.hh>
 #include <dune/gdt/operators/elliptic-cg.hh>
@@ -50,6 +53,8 @@ template< class CoarseFunctionImp, class MicroFunctionImp >
 class PeriodicCorrector;
 template< class CoarseFunctionImp, class MicroFunctionImp >
 class PeriodicCorrectorLocal;
+template< class CoarseFunctionImp, class FineFunctionCurlImp, class FineFunctionIdImp >
+class DeltaCorrectorCurl;
 
 template< class MacroGridViewType, class CellGridType, int polynomialOrder >
 class CurlHMMDiscretization {
@@ -752,6 +757,223 @@ private:
   std::vector< std::unique_ptr< typename CoarseFunctionImp::LocalfunctionType > > local_macro_part_;
   std::vector< std::vector< MicroFunctionImp > > cell_solutions_;
   const std::string type_;
+};
+
+
+template< class CoarseFunctionImp, class FineFunctionCurlImp, class FineFunctionIdImp >
+class DeltaCorrectorCurl
+  : public Dune::Stuff::GlobalFunctionInterface< typename CoarseFunctionImp::EntityType,
+                                                 typename CoarseFunctionImp::DomainFieldType,
+                                                 CoarseFunctionImp::dimDomain,
+                                                 typename CoarseFunctionImp::RangeFieldType,
+                                                 CoarseFunctionImp::dimRange >
+{
+  typedef Dune::Stuff::GlobalFunctionInterface< typename CoarseFunctionImp::EntityType,
+                                                typename CoarseFunctionImp::DomainFieldType, CoarseFunctionImp::dimDomain,
+                                                typename CoarseFunctionImp::RangeFieldType, CoarseFunctionImp::dimRange > BaseType;
+  typedef DeltaCorrectorCurl< CoarseFunctionImp, FineFunctionCurlImp, FineFunctionIdImp >                                 ThisType;
+
+public:
+  using typename BaseType::LocalfunctionType;
+  using typename BaseType::DomainType;
+  using typename BaseType::DomainFieldType;
+  using typename BaseType::RangeType;
+  using typename BaseType::JacobianRangeType;
+
+  static const bool available = false;
+
+  static std::string static_id()
+  {
+    return CoarseFunctionImp::static_id() + ".corrector";
+  }
+
+  DeltaCorrectorCurl(const std::vector< CoarseFunctionImp >& macro_fct,
+                     const std::vector< std::vector< FineFunctionCurlImp > >& curl_cell_solutions,
+                     const std::vector< std::vector< FineFunctionIdImp > >& id_cell_solutions,
+                     const DomainFieldType delta,
+                     const std::string which_part)
+    : macro_function_(macro_fct)
+    , curl_cell_solutions_(curl_cell_solutions)
+    , id_cell_solutions_(id_cell_solutions)
+    , delta_(delta)
+    , part_(which_part)
+  {}
+
+  DeltaCorrectorCurl(const ThisType& /*other*/) = default;
+
+  ThisType& operator=(const ThisType& /*other*/) = delete;
+
+  virtual std::string type() const override final
+  {
+    return CoarseFunctionImp::static_id() + ".corrector." + part_;
+  }
+
+  virtual std::string name() const override final
+  {
+    return CoarseFunctionImp::static_id() + ".corrector." + part_;
+  }
+
+  virtual size_t order() const override final
+  {
+    return macro_function_[0].local_function(*macro_function_[0].space().grid_view().template begin<0>())->order();
+  }
+
+  //only zero order terms, i.e. no delta K_1 is evaluated
+  virtual void evaluate(const DomainType& xx, RangeType& ret) const override final
+  {
+    //clear ret
+    ret *= 0;
+    //tmp storage
+    std::vector< RangeType > macro_total(macro_function_.size(), RangeType(0));
+    //entity search in the macro grid view
+    typedef typename CoarseFunctionImp::SpaceType::GridViewType MacroGridViewType;
+    Dune::Stuff::Grid::EntityInlevelSearch< MacroGridViewType > entity_search(macro_function_[0].space().grid_view());
+    std::vector< Dune::FieldVector< typename MacroGridViewType::ctype, MacroGridViewType::dimension > > global_point(1);
+    global_point[0] = xx;
+    const auto source_entity_ptr = entity_search(global_point);
+    assert(source_entity_ptr.size() == 1);
+    const auto& source_entity_unique_ptr = source_entity_ptr[0];
+    if(source_entity_unique_ptr) {
+      const auto source_entity_ptr1 = *source_entity_unique_ptr;
+      const auto& source_entity = source_entity_ptr1;
+      const auto local_source_point = source_entity.geometry().local(xx);
+      //evaluate macro function and its jacobian
+      for (size_t ii = 0; ii < macro_function_.size(); ++ii)
+        macro_function_[ii].local_function(source_entity)->evaluate(local_source_point, macro_total[ii]);
+    }
+    if (part_ == "real")
+      ret += macro_total[0];
+    else if (part_ == "imag")
+      ret += macro_total[1];
+    else
+      DUNE_THROW(Dune::NotImplemented, "You can only compute real or imag part");
+    //preparation for fine part
+    DomainType yy(xx);
+    yy /= delta_;
+    DomainFieldType intpart;
+    for (size_t ii = 0; ii < CoarseFunctionImp::dimDomain; ++ii) {
+      auto fracpart = std::modf(yy[ii], &intpart);
+      if (fracpart < 0)
+        yy[ii] = 1 + fracpart;
+      else
+        yy[ii] = fracpart;
+    }
+    //now yy is a (global) point in the unit cube
+    //do now the same entity search as before, but for the unit cube grid view
+    //tmp storage
+    std::vector< std::vector< typename FineFunctionIdImp::JacobianRangeType > >
+      id_cell_jacobian(id_cell_solutions_.size(), std::vector< typename FineFunctionIdImp::JacobianRangeType >(id_cell_solutions_[0].size()));
+    typedef typename FineFunctionIdImp::SpaceType::GridViewType FineGridViewType;
+    Dune::Stuff::Grid::EntityInlevelSearch< FineGridViewType > entity_search_fine(id_cell_solutions_[0][0].space().grid_view());
+    std::vector< Dune::FieldVector< typename FineGridViewType::ctype, FineGridViewType::dimension > > fine_point(1);
+    fine_point[0] = yy;
+    const auto source_entity_ptr_fine = entity_search_fine(global_point);
+    assert(source_entity_ptr_fine.size() == 1);
+    const auto& source_entity_unique_ptr_fine = source_entity_ptr_fine[0];
+    if(source_entity_unique_ptr_fine) {
+      const auto source_entity_ptr1 = *source_entity_unique_ptr_fine;
+      const auto& source_entity = source_entity_ptr1;
+      const auto local_source_point = source_entity.geometry().local(yy);
+      //evaluate id cell solutions' jacobian
+      for (size_t ii = 0; ii < id_cell_solutions_.size(); ++ii) {
+        for (size_t jj = 0; jj < id_cell_solutions_[ii].size(); ++jj)
+          id_cell_solutions_[ii][jj].local_function(source_entity)->jacobian(local_source_point, id_cell_jacobian[ii][jj]);
+        if (part_ == "real") {
+          ret.axpy(macro_total[0][ii], id_cell_jacobian[ii][0][0]); //real*real
+          ret.axpy(-1*macro_total[1][ii], id_cell_jacobian[ii][1][0]); //-imag*imag
+        }
+        else if (part_ == "imag") {
+          ret.axpy(macro_total[0][ii], id_cell_jacobian[ii][1][0]); //real*imag
+          ret.axpy(macro_total[1][ii], id_cell_jacobian[ii][0][0]); //imag*real
+        }
+      }
+    }
+  } //evaluate
+
+  //jacobian of K_2 is neglected, which is only correct if you use this for curl compuatations!
+  virtual void jacobian(const DomainType& xx, JacobianRangeType& ret) const override final
+  {
+    //clear ret
+    ret *= 0;
+    //tmp storage
+    std::vector< JacobianRangeType > macro_jacobian(macro_function_.size(), JacobianRangeType(0));
+    std::vector< RangeType > macro_curl(macro_function_.size(), RangeType(0));
+    //entity search in the macro grid view
+    typedef typename CoarseFunctionImp::SpaceType::GridViewType MacroGridViewType;
+    Dune::Stuff::Grid::EntityInlevelSearch< MacroGridViewType > entity_search(macro_function_[0].space().grid_view());
+    std::vector< Dune::FieldVector< typename MacroGridViewType::ctype, MacroGridViewType::dimension > > global_point(1);
+    global_point[0] = xx;
+    const auto source_entity_ptr = entity_search(global_point);
+    assert(source_entity_ptr.size() == 1);
+    const auto& source_entity_unique_ptr = source_entity_ptr[0];
+    if(source_entity_unique_ptr) {
+      const auto source_entity_ptr1 = *source_entity_unique_ptr;
+      const auto& source_entity = source_entity_ptr1;
+      const auto local_source_point = source_entity.geometry().local(xx);
+      //evaluate macro function and its jacobian
+      for (size_t ii = 0; ii < macro_function_.size(); ++ii) {
+        macro_function_[ii].local_function(source_entity)->jacobian(local_source_point, macro_jacobian[ii]);
+        macro_curl[ii][0] = macro_jacobian[ii][2][1] - macro_jacobian[ii][1][2];
+        macro_curl[ii][1] = macro_jacobian[ii][0][2] - macro_jacobian[ii][2][0];
+        macro_curl[ii][2] = macro_jacobian[ii][1][0] - macro_jacobian[ii][0][1];
+      }
+    }
+    if (part_ == "real")
+      ret += macro_jacobian[0];
+    else if (part_ == "imag")
+      ret += macro_jacobian[1];
+    else
+      DUNE_THROW(Dune::NotImplemented, "You can only compute real or imag part");
+    //preparation for fine part
+    DomainType yy(xx);
+    yy /= delta_;
+    DomainFieldType intpart;
+    for (size_t ii = 0; ii < CoarseFunctionImp::dimDomain; ++ii) {
+      auto fracpart = std::modf(yy[ii], &intpart);
+      if (fracpart < 0)
+        yy[ii] = 1 + fracpart;
+      else
+        yy[ii] = fracpart;
+    }
+    //now yy is a (global) point in the unit cube
+    //do now the same entity search as before, but for the unit cube grid view
+    //tmp storage
+    std::vector< std::vector< typename FineFunctionIdImp::JacobianRangeType > >
+      curl_cell_jacobian(curl_cell_solutions_.size(), std::vector< typename FineFunctionIdImp::JacobianRangeType >(curl_cell_solutions_[0].size()));
+    typedef typename FineFunctionIdImp::SpaceType::GridViewType FineGridViewType;
+    Dune::Stuff::Grid::EntityInlevelSearch< FineGridViewType > entity_search_fine(curl_cell_solutions_[0][0].space().grid_view());
+    std::vector< Dune::FieldVector< typename FineGridViewType::ctype, FineGridViewType::dimension > > fine_point(1);
+    fine_point[0] = yy;
+    const auto source_entity_ptr_fine = entity_search_fine(global_point);
+    assert(source_entity_ptr_fine.size() == 1);
+    const auto& source_entity_unique_ptr_fine = source_entity_ptr_fine[0];
+    if(source_entity_unique_ptr_fine) {
+      const auto source_entity_ptr1 = *source_entity_unique_ptr_fine;
+      const auto& source_entity = source_entity_ptr1;
+      const auto local_source_point = source_entity.geometry().local(yy);
+      //evaluate id cell solutions' jacobian
+      for (size_t ii = 0; ii < curl_cell_solutions_.size(); ++ii) {
+        for (size_t jj = 0; jj < curl_cell_solutions_[ii].size(); ++jj)
+          curl_cell_solutions_[ii][jj].local_function(source_entity)->jacobian(local_source_point, curl_cell_jacobian[ii][jj]);
+        if (part_ == "real") {
+          ret.axpy(macro_curl[0][ii], curl_cell_jacobian[ii][0]); //real*real
+          ret.axpy(-1*macro_curl[1][ii], curl_cell_jacobian[ii][1]); //-imag*imag
+        }
+        else if (part_ == "imag") {
+          ret.axpy(macro_curl[0][ii], curl_cell_jacobian[ii][1]); //real*imag
+          ret.axpy(macro_curl[1][ii], curl_cell_jacobian[ii][0]); //imag*real
+        }
+      }
+    }
+  } //jacobian
+
+
+private:
+  const std::vector< CoarseFunctionImp > macro_function_;
+  const std::vector< std::vector< FineFunctionCurlImp > > curl_cell_solutions_;
+  const std::vector< std::vector< FineFunctionIdImp > > id_cell_solutions_;
+  const DomainFieldType delta_;
+  const std::string part_;
 };
 
 
