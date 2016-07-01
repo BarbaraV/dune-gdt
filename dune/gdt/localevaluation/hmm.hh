494e74e3b237597bd4810b5770683e4fcd1bdaba
b_verf01 <b_verf01@uni-muenster.de>
2016-07-01 10:10:25 +0200

    [localevaluation.hmm] added hmm evaluation for the inclusions

--- dune/gdt/localevaluation/hmm.hh
+++ dune/gdt/localevaluation/hmm.hh
@@ -34,6 +34,9 @@ class HMMIdentity;
 template< class FunctionImp, class CellProblemType >
 class HMMEllipticPeriodic;
 
+template< class FunctionImp, class CellProblemType >
+class HMMHelmholtzInclusionPeriodic;
+
 namespace internal {
 
 
@@ -90,6 +93,24 @@ public:
 
   typedef std::function< bool(const FineGridViewType&, const typename CellProblemType::PeriodicEntityType&) > FilterType;
 }; //class HMMEllipticTraits
+
+template< class FunctionType, class CellProblemType >
+class HMMHelmholtzInclusionTraits
+{
+  static_assert(Stuff::is_localizable_function< FunctionType >::value,
+                "FunctionType has to be a localizable function!");
+public:
+  typedef HMMHelmholtzInclusionPeriodic< FunctionType, CellProblemType >            derived_type;
+  typedef typename FunctionType::EntityType                                         EntityType;
+  typedef typename FunctionType::DomainFieldType                                    DomainFieldType;
+  typedef std::tuple< std::shared_ptr< typename FunctionType::LocalfunctionType >,
+                      std::shared_ptr< typename FunctionType::LocalfunctionType > > LocalfunctionTupleType;
+  static const size_t                                                               dimDomain = FunctionType::dimDomain;
+
+  typedef typename CellProblemType::ScalarFct               FineFunctionType;
+  typedef typename CellProblemType::PeriodicViewType        FineGridViewType;
+  typedef typename CellProblemType::CellSolutionStorageType CellSolutionsStorageType;
+}; //class HMMHelmholtzInclusionTraits
 
 
 }//namespace internal
@@ -1012,6 +1033,237 @@ protected:
   const CellSolutionsStorageType& cell_solutions_;
   const FilterType                filter_;
 }; //class HMMEllipticPeriodic
+
+
+/** \brief Class to compute a local (w.r.t. the macroscopic grid) evaluation of the identity part in the HMM for a highly heterogeneous Helmholtz problem
+ *
+ * \tparan FunctionImp The macroscopic type of parameter functions
+ * \tparam CellProblemType The type of cell reconstruction to use for the correctors
+ * \note This class can be used for periodic problems only
+ * \todo implement a general class HMMHelmholtzInclusion
+ */
+template< class FunctionImp, class CellProblemType >
+class HMMHelmholtzInclusionPeriodic
+  : public LocalEvaluation::Codim0Interface< internal::HMMHelmholtzInclusionTraits< FunctionImp, CellProblemType >, 2 >
+{
+public:
+  typedef internal::HMMHelmholtzInclusionTraits< FunctionImp, CellProblemType > Traits;
+  typedef FunctionImp                             FunctionType;
+  typedef typename Traits::LocalfunctionTupleType LocalfunctionTupleType;
+  typedef typename Traits::EntityType             EntityType;
+  typedef typename Traits::DomainFieldType        DomainFieldType;
+  static const size_t                             dimDomain = Traits::dimDomain;
+
+  typedef typename Traits::FineFunctionType         FineFunctionType;
+  typedef typename Traits::FineGridViewType         FineGridViewType;
+  typedef typename Traits::CellSolutionsStorageType CellSolutionsStorageType;
+
+  explicit HMMHelmholtzInclusionPeriodic(const CellProblemType& cell_problem,
+                                         const FineFunctionType& periodic_a_real,
+                                         const FineFunctionType& periodic_a_imag,
+                                         const double wavenumber,
+                                         const bool real_part,
+                                         const FunctionType& macro_a_real,
+                                         const FunctionType& macro_a_imag,
+                                         const CellSolutionsStorageType& cell_solutions)
+    : cell_problem_(cell_problem)
+    , periodic_a_real_(periodic_a_real)
+    , periodic_a_imag_(periodic_a_imag)
+    , wavenumber_(wavenumber)
+    , real_part_(real_part)
+    , macro_a_real_(macro_a_real)
+    , macro_a_imag_(macro_a_imag)
+    , cell_solutions_(cell_solutions)
+  {}
+
+  /// \name Required by LocalEvaluation::Codim0Interface< ..., 2 >
+  /// \{
+
+  LocalfunctionTupleType localFunctions(const EntityType& entity) const
+  {
+    return std::make_tuple(macro_a_real_.local_function(entity), macro_a_imag_.local_function(entity));
+  }
+
+  /**
+   * \brief extracts the local functions and calls the correct order() method
+   */
+  template< class R, size_t rT, size_t rCT, size_t rA, size_t rCA >
+  size_t order(const LocalfunctionTupleType& localFuncs,
+               const Stuff::LocalfunctionSetInterface
+                   < EntityType, DomainFieldType, dimDomain, R, rT, rCT >& testBase,
+               const Stuff::LocalfunctionSetInterface
+                   < EntityType, DomainFieldType, dimDomain, R, rA, rCA >& ansatzBase) const
+  {
+    return order(*std::get< 0 >(localFuncs), *std::get< 1 >(localFuncs), testBase, ansatzBase);
+  }
+
+  /**
+   * \brief extracts the local functions and calls the correct evaluate() method
+   */
+  template< class R, size_t rT, size_t rCT, size_t rA, size_t rCA >
+  void evaluate(const LocalfunctionTupleType& localFuncs,
+                const Stuff::LocalfunctionSetInterface
+                    < EntityType, DomainFieldType, dimDomain, R, rT, rCT >& testBase,
+                const Stuff::LocalfunctionSetInterface
+                    < EntityType, DomainFieldType, dimDomain, R, rA, rCA >& ansatzBase,
+                const Dune::FieldVector< DomainFieldType, dimDomain >& localPoint,
+                Dune::DynamicMatrix< R >& ret) const
+  {
+    evaluate(*std::get< 0 >(localFuncs), *std::get< 1 >(localFuncs), testBase, ansatzBase, localPoint, ret);
+  }
+
+  /// \}
+  /// \name Actual implmentation of order
+  /// \{
+
+  /**
+    * \return localFunction.order()+testBase.order()+ansatzBase.order()
+    */
+  template< class R, size_t rL, size_t rCl, size_t rT, size_t rCT, size_t rA, size_t rCA >
+  size_t order(const Stuff::LocalfunctionInterface< EntityType, DomainFieldType, dimDomain, R, rL, rCl >& localFunctionreal,
+               const Stuff::LocalfunctionInterface< EntityType, DomainFieldType, dimDomain, R, rL, rCl >& localFunctionimag,
+               const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rT, rCT >& testBase,
+               const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rA, rCA >& ansatzBase)
+  const
+  {
+    return boost::numeric_cast< size_t >(std::max(ssize_t(localFunctionreal.order()), ssize_t(localFunctionimag.order())))
+     + testBase.order() + ansatzBase.order();
+  } // ...order(....)
+
+
+  /// \}
+  /// \name Actual implementation of evaluate
+  /// \{
+
+  /**
+    * \brief Computes the HMM elliptic evaluation
+    * \note Contra-intuitively, we first iterate over the microscopic enities and then over the rows and columns (base size of the macroscopic space),
+    * but in most applications the number of entities is large in comparison to the size of the macroscopic space, so this is much faster
+    * \tparam R RangeFieldType
+    */
+
+  template< class R >
+  void evaluate(const Stuff::LocalfunctionInterface< EntityType, DomainFieldType, dimDomain, R, 1, 1 >& localFunctionreal,
+                const Stuff::LocalfunctionInterface< EntityType, DomainFieldType, dimDomain, R, 1, 1 >& localFunctionimag,
+                const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, 1, 1 >& testBase,
+                const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, 1, 1 >& ansatzBase,
+                const Dune::FieldVector< DomainFieldType, dimDomain >& localPoint,
+                Dune::DynamicMatrix< R >& ret) const
+  {
+    typedef typename Stuff::LocalfunctionSetInterface
+        < EntityType, DomainFieldType, dimDomain, R, 1, 1 >::JacobianRangeType JacobianRangeType;
+    typedef typename Stuff::LocalfunctionSetInterface
+        < EntityType, DomainFieldType, dimDomain, R, 1, 1 >::RangeType         RangeType;
+    typedef typename Stuff::LocalfunctionSetInterface
+        < EntityType, DomainFieldType, dimDomain, R, 1, 1 >::RangeFieldType    RangeFieldType;
+    //clear return matrix
+    ret *= 0.;
+    //evaluate testGradient
+    const size_t rows = testBase.size();
+    auto tValue = testBase.evaluate(localPoint);
+    //evaluate ansatz gradient
+    const size_t cols = ansatzBase.size();
+    auto aValue = ansatzBase.evaluate(localPoint);
+    assert(ret.rows()>= rows);
+    assert(ret.cols()>= cols);
+    // product -k^2 u*v
+    for (size_t ii = 0; ii < rows; ++ii) {
+      auto& retRow = ret[ii];
+      for (size_t jj = 0; jj < cols; ++jj) {
+        retRow[jj] -= wavenumber_ * wavenumber_ * (aValue[jj] * tValue[ii]);
+      }
+    }
+    assert(cell_solutions_.size()==1);
+    auto cube_grid_view = cell_solutions_[0]->operator[](0).space().grid_view();
+    auto macro_real_value = localFunctionreal.evaluate(localPoint);
+    auto macro_imag_value = localFunctionimag.evaluate(localPoint);
+    //integrate over the inclusion
+    for (const auto& entity : DSC::entityRange(cube_grid_view) ) {
+      const auto local_a_real = periodic_a_real_.local_function(entity);
+      const auto local_a_imag = periodic_a_imag_.local_function(entity);
+      //get quadrature rule
+      typedef Dune::QuadratureRules< DomainFieldType, dimDomain > VolumeQuadratureRules;
+      typedef Dune::QuadratureRule< DomainFieldType, dimDomain > VolumeQuadratureType;
+      const size_t integrand_order = local_a_real->order() + 2 * (cell_solutions_[0]->operator[](0).local_function(entity)->order());
+      const VolumeQuadratureType& volumeQuadrature = VolumeQuadratureRules::rule(entity.type(), boost::numeric_cast< int >(integrand_order));
+      // evaluate all local solutions and their jacobians in all quadrature points
+      std::vector<RangeType> allLocalSolutionEvaluations_real(volumeQuadrature.size(), RangeType(0.0));
+      std::vector<RangeType> allLocalSolutionEvaluations_imag(volumeQuadrature.size(), RangeType(0.0));
+      std::vector<JacobianRangeType> allLocalSolutionJacobs_real(volumeQuadrature.size(), JacobianRangeType(0.0));
+      std::vector<JacobianRangeType> allLocalSolutionJacobs_imag(volumeQuadrature.size(), JacobianRangeType(0.0));
+      const auto localFunction_real = cell_solutions_[0]->operator[](0).local_function(entity);
+      const auto localFunction_imag = cell_solutions_[0]->operator[](1).local_function(entity);
+      localFunction_real->evaluate(volumeQuadrature, allLocalSolutionEvaluations_real);
+      localFunction_imag->evaluate(volumeQuadrature, allLocalSolutionEvaluations_imag);
+      localFunction_real->jacobian(volumeQuadrature, allLocalSolutionJacobs_real);
+      localFunction_imag->jacobian(volumeQuadrature, allLocalSolutionJacobs_imag);
+      //loop over all quadrature points
+      const auto quadPointEndIt = volumeQuadrature.end();
+      size_t kk = 0;
+      auto ksquared = wavenumber_ * wavenumber_;
+      for (auto quadPointIt = volumeQuadrature.begin(); quadPointIt != quadPointEndIt; ++quadPointIt, ++kk) {
+        const Dune::FieldVector< DomainFieldType, dimDomain > x = quadPointIt->position();
+        //integration factors
+        const double integration_factor = entity.geometry().integrationElement(x);
+        const double quadrature_weight = quadPointIt->weight();
+        auto value_real = (macro_real_value * local_a_real->evaluate(x));
+        auto value_imag = (macro_imag_value * local_a_imag->evaluate(x));
+        //evaluate
+        for (size_t ii = 0; ii < rows; ++ii) {
+          auto& retRow = ret[ii];
+          for (size_t jj = 0; jj < cols; ++jj) {
+            auto correcii_value_real = ksquared * tValue[ii] * allLocalSolutionEvaluations_real[kk];
+            auto correcii_value_imag = ksquared * tValue[ii] * allLocalSolutionEvaluations_imag[kk];
+            auto correcjj_value_real = ksquared * aValue[jj] * allLocalSolutionEvaluations_real[kk];
+            auto correcjj_value_imag = ksquared * aValue[jj] * allLocalSolutionEvaluations_imag[kk];
+            auto correcii_jacob_real = allLocalSolutionJacobs_real[kk][0];
+            correcii_jacob_real *= (ksquared * tValue[ii]);
+            auto correcii_jacob_imag = allLocalSolutionJacobs_imag[kk][0];
+            correcii_jacob_imag *= (ksquared * tValue[ii]);
+            auto correcjj_jacob_real = allLocalSolutionJacobs_real[kk][0];
+            correcjj_jacob_real *= (ksquared * aValue[jj]);
+            auto correcjj_jacob_imag = allLocalSolutionJacobs_imag[kk][0];
+            correcjj_jacob_imag *= (ksquared * aValue[jj]);
+            if (real_part_) {
+              auto tmp_result = value_real * (correcjj_jacob_real * correcii_jacob_real);
+              tmp_result += value_real * (correcjj_jacob_imag * correcii_jacob_imag);
+              tmp_result += value_imag * (correcjj_jacob_real * correcii_jacob_imag);
+              tmp_result -= value_imag * (correcjj_jacob_imag * correcii_jacob_real);
+              tmp_result -= ksquared * (correcjj_value_real * tValue[ii]);
+              tmp_result -= ksquared * (aValue[jj] * correcii_value_real);
+              tmp_result -= ksquared * (correcjj_value_real * correcii_value_real);
+              tmp_result -= ksquared * (correcjj_value_imag * correcii_value_imag);
+              tmp_result *= (quadrature_weight * integration_factor);
+              retRow[jj] += tmp_result;
+            }
+            else {
+              auto tmp_result = value_imag * (correcjj_jacob_imag * correcii_jacob_imag);
+              tmp_result += value_imag * (correcjj_jacob_real * correcii_jacob_real);
+              tmp_result += value_real * (correcjj_jacob_imag * correcii_jacob_real);
+              tmp_result -= value_real * (correcjj_jacob_real * correcii_jacob_imag);
+              tmp_result -= ksquared * (correcjj_value_imag * tValue[ii]);
+              tmp_result += ksquared * (aValue[jj] * correcii_value_imag);
+              tmp_result += ksquared * (correcjj_value_real * correcii_value_imag);
+              tmp_result -= ksquared * (correcjj_value_imag * correcii_value_real);
+              tmp_result *= (quadrature_weight * integration_factor);
+              retRow[jj] += tmp_result;
+            }
+          } //loop over cols
+        } //loop over rows
+      } //loop over quadrature points
+    } //loop over cube entities
+  } // ... evaluate (...)
+
+protected:
+  const CellProblemType&          cell_problem_;
+  const FineFunctionType&         periodic_a_real_;
+  const FineFunctionType&         periodic_a_imag_;
+  const double                    wavenumber_;
+  const bool                      real_part_;
+  const FunctionType&             macro_a_real_;
+  const FunctionType&             macro_a_imag_;
+  const CellSolutionsStorageType& cell_solutions_;
+}; //class HMMHelmholtzInclusionPeriodic
 
 
 }//namespace LocalEvaluation
